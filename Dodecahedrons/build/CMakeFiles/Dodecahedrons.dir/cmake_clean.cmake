1f470775e78705f5ccda046fda996adb8f155087
Arpan Dasgupta <arpandasgupta0@gmail.com>
2021-03-06 21:53:40 +0530

    Fixed file format

---
+++ Dodecahedrons/build/CMakeFiles/Dodecahedrons.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dodecahedrons.dir/source/main.cpp.o"
+  "Dodecahedrons"
+  "Dodecahedrons.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dodecahedrons.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
