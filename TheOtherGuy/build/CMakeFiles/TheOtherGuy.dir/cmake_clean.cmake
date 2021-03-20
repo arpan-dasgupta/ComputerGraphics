8e9ddc121a923d15c25b59b5e4b426d56538d3ce
Arpan Dasgupta <arpandasgupta0@gmail.com>
2021-03-20 19:45:32 +0530

    Fixed Cmakelists

---
+++ TheOtherGuy/build/CMakeFiles/TheOtherGuy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheOtherGuy.dir/source/main.cpp.o"
+  "TheOtherGuy"
+  "TheOtherGuy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheOtherGuy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
